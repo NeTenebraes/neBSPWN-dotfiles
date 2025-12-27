f3db1bbdf5d37be1c7b83abccdc11c46870f9b5d
NeTenebrae <156097208+NeTenebraes@users.noreply.github.com>
2025-12-27 01:33:51 -0500

    Update start.sh

--- Config Files/bspwm/start.sh
+++ Config Files/bspwm/start.sh
@@ -11,16 +11,17 @@ pgrep megasync || megasync &
 pgrep polkit-gnome-authentication-agent-1 || /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 pgrep blueberry-tray || blueberry-tray &
 
+# Desactiva el curso si no lo estás usando.
 pgrep unclutter || unclutter -idle 5 -root &
 
+# Inicia conky
 killall -q conky
-
 (sleep 3 && conky -c /home/netenebrae/.config/conky/conky.conf) &
-# Al final de bspwmrc (aún más seguro)
+
+# Lock screen 
 pgrep xautolock || xautolock -time 5 -locker "betterlockscreen -l" &
 
-
-# 3. Configuración de pantalla (Evita suspensión, apaga a la hora)
+# Configuración de pantalla (Evita suspensión, apaga a la hora)
 xset s off               # Desactiva el protector de pantalla clásico
 xset -dpms               # Resetea DPMS
 xset dpms 0 0 3600 &     # 0 standby, 0 suspend, 3600 (1 hora) para OFF total
